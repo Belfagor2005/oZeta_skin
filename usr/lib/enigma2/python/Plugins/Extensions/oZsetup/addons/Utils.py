0eb0135ef80eaef5cc0904ea7e72189137006dd5
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2023-09-15 21:49:46 +0200

    Utility Fix

--- usr/lib/enigma2/python/Plugins/Extensions/oZsetup/addons/Utils.py
+++ usr/lib/enigma2/python/Plugins/Extensions/oZsetup/addons/Utils.py
@@ -529,20 +529,6 @@ def checkRedirect(url):
     # res = session.get(url)
     # return res
 
-    class TLSAdapter(requests.adapters.HTTPAdapter):
-
-        def init_poolmanager(self, *args, **kwargs):
-            ctx = ssl.create_default_context()
-            ctx.set_ciphers('DEFAULT@SECLEVEL=1')
-            kwargs['ssl_context'] = ctx
-            return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
-
-    session = requests.session()
-    session.mount('https://', TLSAdapter())
-    res = session.get(url)
-    print('TLSAdapter: ', res)
-    return res
-
 
 def freespace():
     try:
